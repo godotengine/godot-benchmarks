6c9041ad7654a78dd3deaa2ef27415321e3a49b5
Hugo Locurcio <hugo.locurcio@hugo.pro>
2022-10-19 22:55:45 +0200

    [WIP] Switch to single-file JSON structure with concatenation
    This is easier to manage on the Hugo side.
    
    This adds timings for startup/shutdown as well.
    
    TODO:
    
    - Revise Hugo backend for the new architecture.
    - Commit example files for testing.

--- run-benchmarks.sh
+++ run-benchmarks.sh
@@ -24,14 +24,18 @@ GODOT_REPO_DIR="$1"
 
 pushd "$GODOT_REPO_DIR"
 
-BEGIN="$(date +%s)"
+# Measure clean build times for debug and release builds (in milliseconds).
+# WARNING: Any untracked files included in the repository will be removed!
+BEGIN="$(date +%s%3N)"
+git clean -dfX
 scons platform=linuxbsd target=template_debug -j$(nproc)
-END="$(date +%s)"
+END="$(date +%s%3N)"
 TIME_TO_BUILD_DEBUG="$((END - BEGIN))"
 
-BEGIN="$(date +%s)"
+BEGIN="$(date +%s%3N)"
+git clean -dfX
 scons platform=linuxbsd target=template_release optimize=speed use_lto=yes -j$(nproc)
-END="$(date +%s)"
+END="$(date +%s%3N)"
 TIME_TO_BUILD_RELEASE="$((END - BEGIN))"
 
 popd
@@ -46,10 +50,7 @@ GODOT_DEBUG="$GODOT_REPO_DIR/bin/godot.linuxbsd.template_release.x86_64"
 
 COMMIT_HASH="$($GODOT_DEBUG --version | rev | cut --delimiter="." --field="1" | rev)"
 DATE="$(date +'%Y-%m-%d')"
-OUTPUT_DIR="web/content/${DATE}_${COMMIT_HASH}"
-mkdir -p "$OUTPUT_DIR"
-# Required for the Hugo page bundle to show up.
-touch "$OUTPUT_DIR/index.md"
+OUTPUT_PATH="web/content/${DATE}_${COMMIT_HASH}.md"
 
 # TODO: Concatenate JSONs into a single JSON file as follows:
 # {
@@ -59,19 +60,58 @@ touch "$OUTPUT_DIR/index.md"
 #  "gpu_intel": {...},
 #  "gpu_nvidia": {...},
 #  "build_times": {"debug": 12345, "release": 12345},
+#  "startup_shutdown_times": {"debug": 12345, "release": 12345}
 # }
 # Figure out if only the `benchmarks` section can be added to each type, with
 # `engine` and `system` being top-level to reduce data duplication (and with all GPUs listed).
 
+# Measure average engine startup + shutdown times over 10 runs (in milliseconds).
+TOTAL=0
+for _ in {0..9}; do
+	BEGIN="$(date +%s%3N)"
+	$GODOT_DEBUG --quit
+	END="$(date +%s%3N)"
+	TOTAL="$((TOTAL + END - BEGIN))"
+done
+TIME_TO_STARTUP_SHUTDOWN_DEBUG="$((TOTAL / 10))"
+
+TOTAL=0
+for _ in {0..9}; do
+	BEGIN="$(date +%s%3N)"
+	$GODOT_RELEASE --quit
+	END="$(date +%s%3N)"
+	TOTAL="$((TOTAL + END - BEGIN))"
+done
+TIME_TO_STARTUP_SHUTDOWN_RELEASE="$((TOTAL / 10))"
+
 # Run CPU benchmarks.
-$GODOT_DEBUG -- --run-benchmarks --exclude-benchmarks="rendering/*" --save-json="$OUTPUT_DIR/cpu_debug.md"
-$GODOT_RELEASE -- --run-benchmarks --exclude-benchmarks="rendering/*" --save-json="$OUTPUT_DIR/cpu_release.md"
+$GODOT_DEBUG -- --run-benchmarks --exclude-benchmarks="rendering/*" --save-json="/tmp/cpu_debug.md"
+$GODOT_RELEASE -- --run-benchmarks --exclude-benchmarks="rendering/*" --save-json="/tmp/cpu_release.md"
 
 # Run GPU benchmarks.
-$GODOT_RELEASE -- --run-benchmarks --include-benchmarks="rendering/*" --save-json="$OUTPUT_DIR/gpu_amd.md"
+$GODOT_RELEASE -- --run-benchmarks --include-benchmarks="rendering/*" --save-json="/tmp/gpu_amd.md"
 # TODO: Uncomment for production use.
-#$GODOT_RELEASE -- --run-benchmarks --include-benchmarks="rendering/*" --save-json="$OUTPUT_DIR/gpu_intel.md"
-#$GODOT_RELEASE -- --run-benchmarks --include-benchmarks="rendering/*" --save-json="$OUTPUT_DIR/gpu_nvidia.md"
+#$GODOT_RELEASE -- --run-benchmarks --include-benchmarks="rendering/*" --save-json="/tmp/gpu_intel.md"
+#$GODOT_RELEASE -- --run-benchmarks --include-benchmarks="rendering/*" --save-json="/tmp/gpu_nvidia.md"
+
+mkdir -p "$(dirname "$OUTPUT_PATH")"
+cat > "$OUTPUT_PATH.md" << EOF
+{
+  "cpu_debug": $(cat /tmp/cpu_debug.md),
+  "cpu_release": $(cat /tmp/cpu_release.md),
+  "gpu_amd": $(cat /tmp/gpu_amd.md),
+  "gpu_intel": $(cat /tmp/gpu_intel.md),
+  "gpu_nvidia": $(cat /tmp/gpu_nvidia.md),
+  "build_times": {
+    "debug": $TIME_TO_BUILD_DEBUG,
+    "release": $TIME_TO_BUILD_RELEASE
+  },
+  "startup_shutdown_times": {
+    "debug": $TIME_TO_STARTUP_SHUTDOWN_DEBUG,
+    "release": $TIME_TO_STARTUP_SHUTDOWN_RELEASE
+  }
+}
+EOF
 
 rm -rf /tmp/godot-benchmarks-results/
 # TODO: Change to godotengine organization URL.
