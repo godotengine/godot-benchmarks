78be36eb822015aa6570c97864c07afbbac7247a
Helehex <helehex@gmail.com>
2024-01-15 06:33:08 -0600

    added empty control tests

---
+++ benchmarks/cs/ControlCS.cs
@@ -0,0 +1,9 @@
+public partial class ControlCS : BenchmarkCS
+{
+    public string description = "An empty test, to act as a control";
+
+    public void BenchmarkControl()
+    {
+
+    }
+}
---
+++ benchmarks/gdscript/control.gd
@@ -0,0 +1,6 @@
+extends Benchmark
+
+var description := "An empty test, to act as a control"
+
+func benchmark_control():
+    pass
