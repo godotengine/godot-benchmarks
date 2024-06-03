d924bd4a9a28a309c39c035683ab0d647220627b
Emmanouil Papadeas <35376950+OverloadedOrama@users.noreply.github.com>
2024-06-04 02:16:49 +0300

    Format nbody

--- benchmarks/gdscript/nbody.gd
+++ benchmarks/gdscript/nbody.gd
@@ -3,6 +3,7 @@ extends Benchmark
 const SOLAR_MASS := 4 * PI * PI
 const DAYS_PER_YEAR := 365.24
 
+
 class Nbody:
 	var x: float
 	var y: float
@@ -12,7 +13,9 @@ class Nbody:
 	var vz: float
 	var mass: float
 
-	func _init(_x: float, _y: float, _z: float, _vx: float, _vy: float, _vz: float, _mass: float) -> void:
+	func _init(
+		_x: float, _y: float, _z: float, _vx: float, _vy: float, _vz: float, _mass: float
+	) -> void:
 		x = _x
 		y = _y
 		z = _z
@@ -20,6 +23,7 @@ class Nbody:
 		vy = _vy
 		vz = _vz
 		mass = _mass
+
 
 var sun := Nbody.new(0.0, 0.0, 0.0, 0.0, 0.0, 0.0, SOLAR_MASS)
 var jupiter := Nbody.new(
@@ -31,32 +35,41 @@ var jupiter := Nbody.new(
 	-6.90460016972063023e-05 * DAYS_PER_YEAR,
 	9.54791938424326609e-04 * SOLAR_MASS
 )
-var saturn := Nbody.new(
-	8.34336671824457987e+00,
-	4.12479856412430479e+00,
-	-4.03523417114321381e-01,
-	-2.76742510726862411e-03 * DAYS_PER_YEAR,
-	4.99852801234917238e-03 * DAYS_PER_YEAR,
-	2.30417297573763929e-05 * DAYS_PER_YEAR,
-	2.85885980666130812e-04 * SOLAR_MASS,
+var saturn := (
+	Nbody
+	. new(
+		8.34336671824457987e+00,
+		4.12479856412430479e+00,
+		-4.03523417114321381e-01,
+		-2.76742510726862411e-03 * DAYS_PER_YEAR,
+		4.99852801234917238e-03 * DAYS_PER_YEAR,
+		2.30417297573763929e-05 * DAYS_PER_YEAR,
+		2.85885980666130812e-04 * SOLAR_MASS,
+	)
 )
-var uranus := Nbody.new(
-	1.28943695621391310e+01,
-	-1.51111514016986312e+01,
-	-2.23307578892655734e-01,
-	2.96460137564761618e-03 * DAYS_PER_YEAR,
-	2.37847173959480950e-03 * DAYS_PER_YEAR,
-	-2.96589568540237556e-05 * DAYS_PER_YEAR,
-	4.36624404335156298e-05 * SOLAR_MASS,
+var uranus := (
+	Nbody
+	. new(
+		1.28943695621391310e+01,
+		-1.51111514016986312e+01,
+		-2.23307578892655734e-01,
+		2.96460137564761618e-03 * DAYS_PER_YEAR,
+		2.37847173959480950e-03 * DAYS_PER_YEAR,
+		-2.96589568540237556e-05 * DAYS_PER_YEAR,
+		4.36624404335156298e-05 * SOLAR_MASS,
+	)
 )
-var neptune := Nbody.new(
-	1.53796971148509165e+01,
-	-2.59193146099879641e+01,
-	1.79258772950371181e-01,
-	2.68067772490389322e-03 * DAYS_PER_YEAR,
-	1.62824170038242295e-03 * DAYS_PER_YEAR,
-	-9.51592254519715870e-05 * DAYS_PER_YEAR,
-	5.15138902046611451e-05 * SOLAR_MASS,
+var neptune := (
+	Nbody
+	. new(
+		1.53796971148509165e+01,
+		-2.59193146099879641e+01,
+		1.79258772950371181e-01,
+		2.68067772490389322e-03 * DAYS_PER_YEAR,
+		1.62824170038242295e-03 * DAYS_PER_YEAR,
+		-9.51592254519715870e-05 * DAYS_PER_YEAR,
+		5.15138902046611451e-05 * SOLAR_MASS,
+	)
 )
 var bodies: Array[Nbody] = [sun, jupiter, saturn, uranus, neptune]
 
@@ -73,10 +86,10 @@ func advance(nbody: int, dt: float) -> void:
 		var bimass := bi.mass
 		for j in range(i + 1, nbody):
 			var bj := bodies[j]
-			var dx := bix-bj.x
-			var dy := biy-bj.y
-			var dz := biz-bj.z
-			var dist2 := dx*dx + dy*dy + dz*dz
+			var dx := bix - bj.x
+			var dy := biy - bj.y
+			var dz := biz - bj.z
+			var dist2 := dx * dx + dy * dy + dz * dz
 			var mag := sqrt(dist2)
 			mag = dt / (mag * dist2)
 			var bm := bj.mass * mag
@@ -104,13 +117,13 @@ func energy(nbody: int) -> float:
 		var vy := bi.vy
 		var vz := bi.vz
 		var bim := bi.mass
-		e = e + (0.5 * bim * (vx*vx + vy*vy + vz*vz))
+		e = e + (0.5 * bim * (vx * vx + vy * vy + vz * vz))
 		for j in range(i + 1, nbody):
 			var bj := bodies[j]
-			var dx := bi.x-bj.x
-			var dy := bi.y-bj.y
-			var dz := bi.z-bj.z
-			var distance := sqrt(dx*dx + dy*dy + dz*dz)
+			var dx := bi.x - bj.x
+			var dy := bi.y - bj.y
+			var dz := bi.z - bj.z
+			var distance := sqrt(dx * dx + dy * dy + dz * dz)
 			e = e - ((bim * bj.mass) / distance)
 	return e
 
