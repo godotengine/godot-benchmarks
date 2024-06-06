02d91717615af56740f729aae26afb06bbaf1be4
Emmanouil Papadeas <35376950+OverloadedOrama@users.noreply.github.com>
2024-06-06 20:46:34 +0300

    Move label.gd to a gui folder, format it and improve static typing

--- benchmarks/gui/label.gd
+++ benchmarks/gui/label.gd
@@ -1,10 +1,14 @@
 extends Benchmark
 
-const LOREM_IPSUM = "Ullam dolorum consequuntur minima neque soluta ab. Est dolores officiis reiciendis omnis eum inventore. Laboriosam quis magni asperiores officia fuga animi qui. Quisquam repudiandae non quisquam vero aut id. Voluptatem occaecati fugiat officia distinctio omnis nesciunt occaecati."
-const FONT_SIZE = 9
+const LOREM_IPSUM := "Ullam dolorum consequuntur minima neque soluta ab. Est dolores officiis reiciendis omnis eum inventore. Laboriosam quis magni asperiores officia fuga animi qui. Quisquam repudiandae non quisquam vero aut id. Voluptatem occaecati fugiat officia distinctio omnis nesciunt occaecati."
+const FONT_SIZE := 9
 
 # Defined to make the label span the entire viewport.
-var label_size := Vector2(ProjectSettings.get_setting("display/window/size/viewport_width"), ProjectSettings.get_setting("display/window/size/viewport_height"))
+var label_size := Vector2(
+	ProjectSettings.get_setting("display/window/size/viewport_width"),
+	ProjectSettings.get_setting("display/window/size/viewport_height")
+)
+
 
 func _init() -> void:
 	test_render_cpu = true
@@ -12,7 +16,7 @@ func _init() -> void:
 
 
 func benchmark_label() -> Label:
-	var label = Label.new()
+	var label := Label.new()
 	label.add_theme_font_size_override("font_size", FONT_SIZE)
 	label.size = label_size
 	label.text = LOREM_IPSUM.repeat(100)
@@ -32,6 +36,6 @@ func benchmark_label_autowrap_word() -> Label:
 
 
 func benchmark_label_autowrap_smart() -> Label:
-	var label = benchmark_label()
+	var label := benchmark_label()
 	label.autowrap_mode = TextServer.AUTOWRAP_WORD_SMART
 	return label
