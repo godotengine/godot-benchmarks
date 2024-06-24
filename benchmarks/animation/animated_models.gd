612edb95e5ee35affda20dbe61793cd0c023496a
Hugo Locurcio <hugo.locurcio@hugo.pro>
2024-06-24 22:46:44 +0200

    Apply suggestions from code review

--- benchmarks/animation/animated_models.gd
+++ benchmarks/animation/animated_models.gd
@@ -98,7 +98,7 @@ class TestScene:
 
 func _init() -> void:
 	test_render_cpu = true
-
+	test_render_gpu = true
 
 func benchmark_animation_state_machine_1000() -> TestScene:
 	return TestScene.new(1000, true).with_state_machine()
