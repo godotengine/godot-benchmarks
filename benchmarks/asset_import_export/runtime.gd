ab8f73e2895cb33c11897565b7645bb9737b0481
Emmanouil Papadeas <35376950+OverloadedOrama@users.noreply.github.com>
2024-06-25 01:01:34 +0300

    Update runtime.gd

--- benchmarks/asset_import_export/runtime.gd
+++ benchmarks/asset_import_export/runtime.gd
@@ -1,5 +1,6 @@
 extends Benchmark
 
+const TEMP_PATH := "user://tmp"
 const IMAGE_PATH := "res://supplemental/images"
 const AUDIO_PATH := "res://supplemental/audio"
 
@@ -20,18 +21,18 @@ func benchmark_export_gltf() -> void:
 	var gltf_document := GLTFDocument.new()
 	var gltf_state := GLTFState.new()
 	gltf_document.append_from_file("res://thirdparty/sponza/sponza.glb", gltf_state)
-	DirAccess.make_dir_absolute("user://tmp")
+	DirAccess.make_dir_absolute(TEMP_PATH)
 	gltf_document.write_to_filesystem(gltf_state, "user://tmp/sponza.glb")
-	DirAccess.remove_absolute("user://tmp/sponza.glb")
-	DirAccess.remove_absolute("user://tmp")
+	DirAccess.remove_absolute(TEMP_PATH.path_join("sponza.glb"))
+	DirAccess.remove_absolute(TEMP_PATH)
 
 
-func benchmark_import_images() -> void:
+func benchmark_import_webp_images() -> void:
 	for i in 200:
 		load(IMAGE_PATH.path_join("%d.webp" % i))
 
 
-func benchmark_import_audio() -> void:
+func benchmark_import_ogg_audio() -> void:
 	var dir := DirAccess.open(AUDIO_PATH)
 	for file in dir.get_files():
 		if not file.get_extension() == "ogg":
