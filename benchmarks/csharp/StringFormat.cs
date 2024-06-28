f3350bb8ea08b11201665e77b52ab5ac1a975bb6
Emmanouil Papadeas <35376950+OverloadedOrama@users.noreply.github.com>
2024-06-28 12:39:05 +0300

    Fix C# string format benchmarks

--- benchmarks/csharp/StringFormat.cs
+++ benchmarks/csharp/StringFormat.cs
@@ -1,5 +1,6 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 public partial class StringFormat : Benchmark
 {
@@ -7,6 +8,7 @@ public partial class StringFormat : Benchmark
     const int ITERATIONS = 1_000_000;
     const string ENGINE_NAME = "Godot";
     Godot.Collections.Dictionary FORMAT_DICT = new Godot.Collections.Dictionary(){{"engine", ENGINE_NAME}};
+    //Dictionary<string, string> FORMAT_DICT = new Dictionary<string, string>() {{"engine", engine_name}};
 
     string engine_name = "Godot";
     int some_integer = 123456;
@@ -35,13 +37,13 @@ public partial class StringFormat : Benchmark
 
     private void BenchmarkSimpleConstantMethod() {
         for (int i = 0; i < ITERATIONS; i++) {
-            String.Format("Hello {engine}!", new Godot.Collections.Dictionary(){{"engine", ENGINE_NAME}});
+            String.Format("Hello {0}!", new Godot.Collections.Dictionary(){{"engine", ENGINE_NAME}}["engine"]);
         }
     }
 
     private void BenchmarkSimpleConstantMethodConstantDict() {
         for (int i = 0; i < ITERATIONS; i++) {
-            String.Format("Hello {engine}!", FORMAT_DICT);
+            String.Format("Hello {0}!", FORMAT_DICT["engine"]);
         }
     }
 
@@ -59,7 +61,9 @@ public partial class StringFormat : Benchmark
 
     private void BenchmarkSimpleVariableMethod() {
         for (int i = 0; i < ITERATIONS; i++) {
-            String.Format("Hello {engine}!", new Godot.Collections.Dictionary(){{"engine", engine_name}});
+            String.Format("Hello {0}!", new Godot.Collections.Dictionary(){{"engine", engine_name}}["engine"]);
+            //Dictionary<string, string> d = new Dictionary<string, string>() {{"engine", engine_name}};
+            //String.Format("Hello {0}!", d["engine"]);
         }
     }
 
@@ -77,12 +81,15 @@ public partial class StringFormat : Benchmark
 
     private void BenchmarkComplexVariableMethod() {
         for (int i = 0; i < ITERATIONS; i++) {
-            String.Format("Hello {engine}!\nA few examples of formatting: {an_integer}, {a_float}, {a_vector2}", new Godot.Collections.Dictionary(){
+            Godot.Collections.Dictionary temp_dict = new Godot.Collections.Dictionary(){
                     {"engine", engine_name},
                     {"an_integer", some_integer},
                     {"a_float", some_float.ToString().PadDecimals(2)},
                     {"a_vector2i", some_vector2i},
-            });
+            };
+            String.Format(
+                "Hello {0}!\nA few examples of formatting: {1}, {2}, {3}", temp_dict["engine"], temp_dict["an_integer"], temp_dict["a_float"], temp_dict["a_vector2i"]
+            );
         }
     }
 }
